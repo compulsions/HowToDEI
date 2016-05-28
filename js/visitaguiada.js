ce727453da7199ccecba147259e4a2a975b5b05e
Mariana <marianne.lage@gmail.com>
2016-05-28 23:17:38 +0100

    Corrige um erro, Adiciona função aos botões do index

--- js/visitaguiada.js
+++ js/visitaguiada.js
@@ -6,6 +6,17 @@ $(function() {
     $("#visita_guiada").click( function(){
         $(".mapa").hide(800);
         $("#visita_guiada").hide(800);
+        $("#visita_experiente").hide(800);
+        $(".texto_visita").show(800);
+
+        //setTimeout("window.location.href = "guia.html"", 2000);
+        setTimeout(function(){ window.location = "guia.html"; }, 5000);
+    });
+    
+     $("#visita_experiente").click( function(){
+        $(".mapa").hide(800);
+        $("#visita_guiada").hide(800);
+        $("#visita_experiente").hide(800);
         $(".texto_visita").show(800);
 
         //setTimeout("window.location.href = "guia.html"", 2000);
