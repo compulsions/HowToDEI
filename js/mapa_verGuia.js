09677ba5b25f7db27fbceb68b0e496de316aef98
Mariana <marianne.lage@gmail.com>
2016-05-28 23:32:14 +0100

    Corrige erros, melhora formatação Js do mapa

--- js/mapa_verGuia.js
+++ js/mapa_verGuia.js
@@ -10,7 +10,7 @@ $(function() {
     $("#InfoImportantes").click( function(){
         $('.pontos_visivel').removeClass('pontos_visivel');
         
-         $("#floor1").click( function(){
+        $("#floor1").click( function(){
             $('.pontos_visivel').removeClass('pontos_visivel');
             $('#locaisImportantes_pontosP1').addClass('pontos_visivel');
              cobaia = $('.pontos_visivel');
@@ -20,25 +20,22 @@ $(function() {
        $("#floor4").click( function(){
            $('.pontos_visivel').removeClass('pontos_visivel');
             $('#locaisImportantes_pontosP4').addClass('pontos_visivel');
-    });
+       });
         
        $("#floor5").click( function(){
            $('.pontos_visivel').removeClass('pontos_visivel');
-    });
+       });
         
         $("#floor6").click( function(){ 
             $('.pontos_visivel').removeClass('pontos_visivel');
             $('#locaisImportantes_pontosP6').addClass('pontos_visivel');
-    });
+        });
        
 //Mudar o estilo dos botoes        
-    document.getElementById("InfoImportantes").style.color = "#6f6f6f";
-    document.getElementById("LocaisEstudo").style.color = "#333";
-        
-    document.getElementById("LocaisConvivio").style.color = "#333";
-        
-    document.getElementById("InfoWC").style.color = "#333";
-    
+        document.getElementById("InfoImportantes").style.color = "#6f6f6f";
+        document.getElementById("LocaisEstudo").style.color = "#333";
+        document.getElementById("LocaisConvivio").style.color = "#333";
+        document.getElementById("InfoWC").style.color = "#333";  
     });
     
     ////////////////////////////////////////////////////////
@@ -48,104 +45,95 @@ $(function() {
         $('.pontos_visivel').removeClass('pontos_visivel');
         
         
-        
-       $("#floor1").click( function(){
+        $("#floor1").click( function(){
             $('.pontos_visivel').removeClass('pontos_visivel');
+       });
 
         
        $("#floor4").click( function(){
            $('.pontos_visivel').removeClass('pontos_visivel');
            $('#locaisEstudo_PontosP4').addClass('pontos_visivel');
-    });
+        });
         
        $("#floor5").click( function(){ 
            $('.pontos_visivel').removeClass('pontos_visivel');
            $('#locaisEstudo_PontosP5').addClass('pontos_visivel');
  
-    });
+       });
         
-        $("#floor6").click( function(){ 
+        $("#floor6").click( function(){
             $('.pontos_visivel').removeClass('pontos_visivel');
     });
 
     //Mudar o estilo dos botoes        
-    document.getElementById("InfoImportantes").style.color = "#333";
-    document.getElementById("LocaisEstudo").style.color = "#6f6f6f";
-        
-    document.getElementById("LocaisConvivio").style.color = "#333";
-        
-    document.getElementById("InfoWC").style.color = "#333";
+        document.getElementById("InfoImportantes").style.color = "#333";
+        document.getElementById("LocaisEstudo").style.color = "#6f6f6f";      
+        document.getElementById("LocaisConvivio").style.color = "#333";     
+        document.getElementById("InfoWC").style.color = "#333";
     });
 
     ///////////////////////////////////////////////////////////
     
 //Alterar localizações conforme categoria
     $("#LocaisConvivio").click( function(){
-         $('.pontos_visivel').removeClass('pontos_visivel');
-    });
+        $('.pontos_visivel').removeClass('pontos_visivel');
         
-       $("#floor1").click( function(){
-           $('.pontos_visivel').removeClass('pontos_visivel');
-    });
+        $("#floor1").click( function(){
+            $('.pontos_visivel').removeClass('pontos_visivel');
+        });
         
        $("#floor4").click( function(){
            $('.pontos_visivel').removeClass('pontos_visivel');
            $('#locaisConvivio_pontos').addClass('pontos_visivel');
 
-    });
+       });
         
        $("#floor5").click( function(){ 
-            $('.pontos_visivel').removeClass('pontos_visivel');
-    });
+           $('.pontos_visivel').removeClass('pontos_visivel');
+       });
         
         $("#floor6").click( function(){ 
             $('.pontos_visivel').removeClass('pontos_visivel');
-    });
+        });
     
     //Mudar o estilo dos botoes        
-    document.getElementById("InfoImportantes").style.color = "#333";
-    document.getElementById("LocaisEstudo").style.color = "#333";
-        
-    document.getElementById("LocaisConvivio").style.color = "#6f6f6f";
-        
-    document.getElementById("InfoWC").style.color = "#333";
+        document.getElementById("InfoImportantes").style.color = "#333";
+        document.getElementById("LocaisEstudo").style.color = "#333";      
+        document.getElementById("LocaisConvivio").style.color = "#6f6f6f";      
+        document.getElementById("InfoWC").style.color = "#333";
     });
     
     
     //////////////////////////////////////////////////////////
     
 //Alterar localizações conforme categoria
-    $("#InfoWC").click( function(){   
+    $("#InfoWC").click( function(){  
         $('.pontos_visivel').removeClass('pontos_visivel');
         
-       $("#floor1").click( function(){
-           $('.pontos_visivel').removeClass('pontos_visivel');
-    });
+        $("#floor1").click( function(){
+            $('.pontos_visivel').removeClass('pontos_visivel');
+        });
         
-       $("#floor4").click( function(){
+        $("#floor4").click( function(){
            $('.pontos_visivel').removeClass('pontos_visivel');
            $('#wcs_pontosP4').addClass('pontos_visivel');
-    });
+        });
         
        $("#floor5").click( function(){ 
-            $('.pontos_visivel').removeClass('pontos_visivel');
+           $('.pontos_visivel').removeClass('pontos_visivel');
            $('#wcs_pontosP5').addClass('pontos_visivel');
-    });
+        });
         
         $("#floor6").click( function(){ 
             $('.pontos_visivel').removeClass('pontos_visivel');
-           $('#wcs_pontosP5').addClass('pontos_visivel');
-    });
+            $('#wcs_pontosP5').addClass('pontos_visivel');
+        });
         
    //Mudar o estilo dos botoes        
-    document.getElementById("InfoImportantes").style.color = "#333";
-    document.getElementById("LocaisEstudo").style.color = "#333";
-        
-    document.getElementById("LocaisConvivio").style.color = "#333";
-        
-    document.getElementById("InfoWC").style.color = "#6f6f6f";
-   
-   
+        document.getElementById("InfoImportantes").style.color = "#333";
+        document.getElementById("LocaisEstudo").style.color = "#333";   
+        document.getElementById("LocaisConvivio").style.color = "#333";     
+        document.getElementById("InfoWC").style.color = "#6f6f6f";
     });
     
     /////////////// Alteração entre os mapas //////////////
@@ -158,23 +146,23 @@ $(function() {
             $('.pontos_visivel').removeClass('pontos_visivel');
             $('#locaisImportantes_pontosP1').addClass('pontos_visivel');
 
-          });
+        });
         
         
         $("#LocaisEstudo").click( function(){
             $('.pontos_visivel').removeClass('pontos_visivel');
-          });
+        });
         
         $("#LocaisConvivio").click( function(){
             $('.pontos_visivel').removeClass('pontos_visivel');
-          });
+        });
         $("#InfoWC").click( function(){
             $('.pontos_visivel').removeClass('pontos_visivel');
-          });
+        });
     });
     
     $("#floor4").click( function(){
-         $('.mapa_guiacategorias').css("background-image", "url(/img/m2.png)");
+        $('.mapa_guiacategorias').css("background-image", "url(/img/m2.png)");
         
         $("#InfoImportantes").click( function(){
             $('.pontos_visivel').removeClass('pontos_visivel');
@@ -237,4 +225,3 @@ $(function() {
             $('#wcs_pontosP5').addClass('pontos_visivel');
           });
     });
-});
